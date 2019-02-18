f89f34ffafd69fe6da7257b2791ea8f16803b0e9
Visperi <niksuposti@gmail.com>
2019-02-18 18:33:40 +0200

    Add options to leave output fields blank

--- main.py
+++ main.py
@@ -27,25 +27,26 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("FFmpeGUI")
-        self.setGeometry(300, 300, 450, 170)
+        self.setGeometry(300, 300, 450, 160)
         self.icon = QIcon("resources\\main_icon.ico")
         self.make_main_layout()
         self.add_widget_actions()
 
     def make_main_layout(self):
-        main_interface = Window()
+        main_interface = QWidget()
 
         # Make all the labels, buttons and boxes in main interface
-        self.input_path = QLineEdit()
+        self.input_file_path = QLineEdit()
         self.input_path_browse = QPushButton("Browse")
         input_label = QLabel("Input file")
 
-        self.output_path = QLineEdit()
+        self.output_path_field = QLineEdit()
+        self.output_path_field.setPlaceholderText("Leave this field blank to use the input directory")
         self.output_path_browse = QPushButton("Browse")
         output_label = QLabel("Output directory")
 
         self.new_filename = QLineEdit()
-        self.new_filename.setPlaceholderText("Leave this blank to use the original file name")
+        self.new_filename.setPlaceholderText("Leave this field blank to use the original file name")
         new_filename_label = QLabel("New file name")
 
         self.bitrate_box = QComboBox()
@@ -54,7 +55,7 @@ class MainWindow(QMainWindow):
         bitrate_unit_label = QLabel("kbps")
 
         self.output_format_box = QComboBox()
-        self.output_format = "MP3"
+        self.output_format = ".MP3"
         output_format_label = QLabel("Format")
 
         self.make_mono_box = QCheckBox()
@@ -62,7 +63,7 @@ class MainWindow(QMainWindow):
         make_mono_lbl = QLabel("Make mono")
 
         self.settings_btn = QPushButton("Settings")
-        self.start_btn = QPushButton("Start")
+        self.start_btn = QPushButton("Convert")
         self.exit_btn = QPushButton("Exit")
 
         # Add labels, buttons and boxes into grids. The main interface is separated into two grids so it's easier to
@@ -71,10 +72,10 @@ class MainWindow(QMainWindow):
         lower_grid = QGridLayout()
 
         upper_grid.addWidget(input_label, 0, 0)
-        upper_grid.addWidget(self.input_path, 0, 1)
+        upper_grid.addWidget(self.input_file_path, 0, 1)
         upper_grid.addWidget(self.input_path_browse, 0, 2)
         upper_grid.addWidget(output_label, 1, 0)
-        upper_grid.addWidget(self.output_path, 1, 1)
+        upper_grid.addWidget(self.output_path_field, 1, 1)
         upper_grid.addWidget(self.output_path_browse, 1, 2)
         upper_grid.addWidget(new_filename_label, 2, 0)
         upper_grid.addWidget(self.new_filename, 2, 1)
@@ -114,7 +115,7 @@ class MainWindow(QMainWindow):
         self.bitrate_box.addItems(output_bitrates)
         self.output_format_box.addItems(output_formats)
 
-        self.input_path_browse.clicked.connect(self.set_input_path)
+        self.input_path_browse.clicked.connect(self.set_input_file_path)
         self.output_path_browse.clicked.connect(self.set_output_path)
         self.settings_btn.clicked.connect(self.open_settings_window)
         self.start_btn.clicked.connect(self.start_ffmpeg_convert)
@@ -123,13 +124,13 @@ class MainWindow(QMainWindow):
         self.bitrate_box.activated.connect(self.set_bitrate)
         self.output_format_box.activated.connect(self.set_output_format)
 
-    def set_input_path(self):
+    def set_input_file_path(self):
         input_file_path = QFileDialog.getOpenFileName(self, "Choose an input file")[0]
-        self.input_path.setText(input_file_path)
+        self.input_file_path.setText(input_file_path)
 
     def set_output_path(self):
         output_directory_path = QFileDialog.getExistingDirectory(self, "Choose an output directory")
-        self.output_path.setText(output_directory_path)
+        self.output_path_field.setText(output_directory_path)
 
     def open_settings_window(self):
         # TODO: Settings window with testing mode check box and default paths
@@ -139,41 +140,40 @@ class MainWindow(QMainWindow):
         self.make_mono_state = self.make_mono_box.isChecked()
 
     def set_output_format(self):
-        self.output_format = self.output_format_box.currentText()
+        self.output_format = f".{self.output_format_box.currentText()}"
 
     def set_bitrate(self):
         self.bitrate = self.bitrate_box.currentText()
 
     def start_ffmpeg_convert(self):
-        input_path = self.input_path.text()
-        output_dir_path = self.output_path.text()
+        input_path = self.input_file_path.text()
+        output_dir_path = self.output_path_field.text()
         make_mono = self.make_mono_state
         output_format = self.output_format
         bitrate = self.bitrate
-        new_output_filename = self.new_filename.text()
-        custom_output_format = os.path.splitext(new_output_filename)[1]
+        renamed_output_filename = self.new_filename.text()
+        manual_output_format = os.path.splitext(renamed_output_filename)[1]
 
-        if not new_output_filename:
+        if not output_dir_path:
+            output_dir_path = os.path.split(input_path)[0]
+        if not renamed_output_filename:
             input_file_name = os.path.split(input_path)[1]
             input_file_ext = os.path.splitext(input_path)[1]
             input_file_name = input_file_name.strip(input_file_ext)
-            output_path = os.path.join(output_dir_path, f"{input_file_name}.{output_format.lower()}")
-        elif custom_output_format and custom_output_format != ".":
-            output_path = os.path.join(output_dir_path, f"{new_output_filename}")
-        else:
-            output_path = os.path.join(output_dir_path, f"{new_output_filename}.{output_format.lower()}")
+            output_file_name = input_file_name
 
-        convert.convert_file(input_path, output_path, make_mono, bitrate, testing_mode=False)
+        elif manual_output_format and manual_output_format != ".":
+            output_file_name = os.path.splitext(renamed_output_filename)[0]
+            output_format = manual_output_format
+
+        else:
+            output_file_name = renamed_output_filename
+
+        output_path = os.path.join(output_dir_path, f"{output_file_name}{output_format.lower()}")
+        convert.convert_file(input_path, output_path, make_mono, bitrate, testing_mode=True)
 
     def close_program(self):
         self.close()
-
-
-# noinspection PyArgumentList
-class Window(QWidget):
-
-    def __init__(self):
-        super().__init__()
 
 
 if __name__ == '__main__':
